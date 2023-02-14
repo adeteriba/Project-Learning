a1cadf8fc41c4b167dd1a651b352b2f3a47d5cb7
Ade Teriba <adekunleteriba@hotmail.com>
2023-02-14 19:11:54 +0000

    LandasPossible

---
+++ Leetcode/LandasPossible.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+class LandasPossible {
+    public static void main (String[] args){
+        System.out.println(extracted());
+    }
+
+    private static String extracted() {
+        return "LandasPossible";
+    }
+
+
+
+}

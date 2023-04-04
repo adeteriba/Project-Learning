65c2b1757b8103e621be2c79c66c3f6ec437aecb
Ade Teriba <adekunleteriba@hotmail.com>
2023-04-04 08:41:49 +0100

    Currenct Project

---
+++ Leetcode/thirdMaximumNumber.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class thirdMaximumNumber {
+    public static void main(String [] args){
+
+    }
+
+    public int thirdMax(int[] nums) {
+        return 0;
+
+    }
+    
+}
